3fe16d408cc8fea9d0249f35efec1398fc369241
kotaro-mt <137480503+kotaro-mt@users.noreply.github.com>
2025-10-17 14:05:27 +0900

    変数など

--- main/main.ino
+++ main/main.ino
@@ -2,12 +2,30 @@
 #include <Pushbutton.h> // プッシュボタンプッシュボタンライブラリのヘッダーファイル
 #include <Wire.h> // 通信のヘッダーファイル
 
+ZumoMotor motors;
+Pushbutton button(ZUMO_BUTTON);
+
+int mode = 0; // 動作モード
+int motorL, motorR; // モーター速度
+float red,green,blue; // RGB値
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  setupCompass();
+
+  //カラーセンサーのキャリブレーション
+  button.waitForButton();
+  CalibrationColorSensor();
+
+  //地磁気センサのキャリブレーション
+  button.waitForButton();
+  calibrationCompass()
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  getRGB(red, green, blue); // RGB値の取得
+  Serial.println("R:" + String(red) + " G:" + String(green) + " B:" + String(blue));
 
 }
