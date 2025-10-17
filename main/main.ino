91afb2100889e45050e0110020e5e8c6f334b862
kotaro-mt <137480503+kotaro-mt@users.noreply.github.com>
2025-10-17 15:30:56 +0900

    方角の取得

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,7 @@
 #include <ZumoMotors.h> // モーターライブラリのヘッダーファイル
 #include <Pushbutton.h> // プッシュボタンプッシュボタンライブラリのヘッダーファイル
 #include <Wire.h> // 通信のヘッダーファイル
+#include <LSM303.h> // LSM303ライブラリのヘッダーファイル
 
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
@@ -9,6 +10,7 @@ int mode = 0; // 動作モード
 int motorL, motorR; // モーター速度
 float red,green,blue; // RGB値
 int dist; // オブジェクトまでの距離
+float angle;  //向いている方角
 
 void setup() {
   Serial.begin(9600);
@@ -21,17 +23,20 @@ void setup() {
   CalibrationColorSensor();
 
   //地磁気センサのキャリブレーション
-  // button.waitForButton();
-  // calibrationCompass()
+  button.waitForButton();
+  calibrationCompass();
 }
 
 void loop() {
 
   getRGB(red, green, blue); // RGB値の取得
   dist = distance(); // オブジェクトまでの距離の取得
+  angle = averageHeading(); // 向いている方角の取得
+
 
   Serial.println("R:" + String(red) + " G:" + String(green) + " B:" + String(blue));
   Serial.println("Distance:" + String(dist) + "cm");
+  Serial.println("Angle:" + String(angle) + "deg");
   delay(500);
 
 }
