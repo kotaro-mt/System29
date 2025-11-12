9491bbdb00257c60a910f2c8fc020df81348279c
angoro <erkmbk729@icloud.com>
2025-11-13 01:04:35 +0900

    敵陣ロボ

--- main/frobot.ino
+++ main/frobot.ino
@@ -1,95 +1,148 @@
-// // motorR = 200;
-// //motorL = 217
+// motorR = 200;
+//motorL = 217
 
-// unsigned long f_time;
-// float f_angle;
-// int f_mode = -1;
-// int erea;
+unsigned long f_time;
+float f_angle;
+int f_mode = -1;
+int erea;
 
-// void forward_robot() {
-//  // 敵陣用ロボのロール
-//  if (role == FORWARD) {
-//    switch (f_mode) {
-//      case -1: // 敵陣まで移動：赤陣地の場合
-//       f_angle = angle; // 最初の方向を記録
-//       f_mode = 1;
-//       break;
+void forward_robot() {
+ // 敵陣用ロボのロール
+  if (role == FORWARD) {
+    switch (f_mode) {
+      case -1: // 敵陣まで移動：赤陣地の場合
+        f_angle = angle; // 最初の方向を記録
+        f_mode = 1;
+        break;
     
-//      case 0:
-//       if (80 < angle - f_angle < 100) { //敵陣方向に回転
-//         motorR = -200;
-//         motorL = 217;
-//       } else {
-//         f_mode = 1;
-//         f_time = millis();
-//       }
-//       break;
+      case 0:
+        if (80 < angle - f_angle < 100) { //敵陣方向に回転
+          motorR = -200;
+          motorL = 217;
+        } else {
+          f_mode = 1;
+          f_time = millis();
+        }
+        break;
 
-//      case 1: // 敵陣まで直進
-//       motorR = 200;
-//       motorL = 217;
-//       if(milli() - f_time > 6000) { // 6秒経過したら探索へ移行
-//         mode = 3; // 探索へ移行
-//         f_time = millis();
-//       }
-//       break;
+      case 1: // 敵陣まで直進
+        motorR = 200;
+        motorL = 217;
+        if(milli() - f_time > 6000) { // 6秒経過したら探索へ移行
+          f_mode = 3; // 探索へ移行
+          f_time = millis();
+        }
+        break;
 
-//      case 2: // 探索-回転
-//       if (millis() - f_time >= 1000) { // 1秒経過したら再直進
-//         f_mode = 3; // 探索-直進
-//         b_time = millis();
-//       } else {
-//         motorL = -150;  // 左旋回
-//         motorR = 150;
-//       }
-//         // 途中で物体を発見した場合
-//         if (dist > 0) {
-//           if (!targetAngleRecorded) {     // まだ角度を記録していなければ
-//             f_angle = angle;             // 現在角度を記録
-//             targetAngleRecorded = true;
-//           }
-//           f_mode = 4;                      // CHECKへ
-//           f_time = millis();
-//         }
-//       break;
+      case 2: // 探索-回転
+        if (millis() - f_time >= 1000) { // 1秒経過したら再直進
+          f_mode = 3; // 探索-直進
+          b_time = millis();
+        } else {
+          motorL = -150;  // 左旋回
+          motorR = 150;
+        }
+        // 途中で物体を発見した場合
+        if (dist > 0) {
+          if (!targetAngleRecorded) {     // まだ角度を記録していなければ
+            f_angle = angle;             // 現在角度を記録
+            targetAngleRecorded = true;
+          }
+          f_mode = 4;                      // CHECKへ
+          f_time = millis();
+        }
+        break;
 
-//     case 3: // 探索-直進
-//       motorR = 200;
-//       motorL = 217;
-//         if (millis() - f_time >= 1000) { // 1秒後に探索へ
-//           f_mode = 2;
-//           f_time = millis();
-//         }
+      case 3: // 探索-直進
+        motorR = 200;
+        motorL = 217;
+          if (millis() - f_time >= 1000) { // 1秒後に探索へ
+            f_mode = 2;
+            f_time = millis();
+          }
 
-//         if (color == BLACK || color == RED || color == BLUE) {
-//           f_mode = 7; // 色反応処理
-//           f_time = millis();
-//         }
-//       break;
-    
-//     case 4: // CHECK
-//       if (Check(dist, checkStartTime, prevDist, changeCount, 500, 3)) {
-//         f_mode = 5;
-//         f_time = millis();
-//       } else if (millis() - checkStartTime >= 500) {
-//         f_mode = 2;
-//         f_time = millis();
-//       }
-//       break;
+          if (color == BLACK || color == RED || color == BLUE) {
+            f_mode = 7; // 色反応処理
+            f_time = millis();
+          }
+        break;
+      
+      case 4: // CHECK 静止反転
+        if (Check(dist, checkStartTime, prevDist, changeCount, 500, 3)) {
+          f_mode = 5;
+          f_time = millis();
+        } else if (millis() - checkStartTime >= 500) {
+          f_mode = 2;
+          f_time = millis();
+        }
+        break;
 
-//     case 99:
-//       color_move(color, f_time);
-//       break;
+      case 5: // キャッチ
+          Serial.println("[Back] HIT");
+          motorL = 217;
+          motorR = 200;
+          if (dist <= 5) {
+            f_mode = 6; // 運搬
+            f_angle = angle; // 角度を記録（どっちの壁に運ぶのが楽か算出するため）
+            f_time = millis();
+            Serial.println("[Back] 十分接近 → RETURNへ");
+          }
+          break;
+      
+      case 6: //運搬-回転
+        if (80 < angle < 100) { // 黒の壁を向いたら直進
+          motorR = 200; motorL = 217;
+          if (color == BLACK) { // 黒を認識したら
+            f_time = millis();
+            f_mode = 100; // 外に出す処理
+          }
+        } else { // 黒の壁を向くまで回転
+          motorR = -200;
+          motorL = 217;
+        }
+        break;
 
-//      case 100: // 投げる
-//       if ( f_angle)
-//        motorR = 400;
-//        motorL = 400;
+      case 99:
+        color_move(color, f_time);
+        break;
 
-//        if (color == BLACK) {
-//          f_mode = 1;
-//        }
-//        break;
-//    }
-//  }
-// }
+      case 100: // 投げるor押し出し
+        if(millis() - f_time < 1500) { // 1.5秒間直進
+          motorR = 200; motorL = 217;
+        } else if (1500 <= millis() - f_time < 3000){
+          motorR = -200; motorL = -217; // 1.5秒間後退
+          f_angle = angle;
+        } else {
+          if (260 < angle - f_angle < 280) {
+            f_mode = 3; // 探索-直進へ移行
+          } else { // 反方向まで回転
+            motorR = -200;
+            motorL = -217;
+          }
+        }
+        break;
+
+      // case 6: // 運搬-自陣ver
+      //   if (160 < angle < 200) { // 運搬
+      //     motorR = 200;  motorL = 217;
+      //   } else { // 自陣のほうを向く
+      //     motorR = 200;  motorL = -217;
+      //     if (color == RED) { // 赤を踏んだら押し出し
+      //       f_time = millis();
+      //       f_mode = 7;
+      //     }
+      //   }
+      //   break;
+
+      // case 7:
+      //   if (millis() - f_time < 1000) {
+      //       motorR = 200;  motorL = 217;
+      //   } else {
+      //     motorR = 200;  motorL = -217;
+      //     if (0 < f_angle - angle < 20) {
+      //       f_mode = 3;
+      //     }
+      //   }
+    }
+  }
+}
